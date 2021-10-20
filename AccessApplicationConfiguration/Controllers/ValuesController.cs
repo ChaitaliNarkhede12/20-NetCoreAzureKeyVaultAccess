4c36471081e5de6b06269275954a5dc26b0ec263
Chaitali Narkhede <Chaitali_Narkhede@epam.com>
2021-10-20 17:43:31 +0530

    Initial Check-in

---
+++ AccessApplicationConfiguration/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AccessApplicationConfiguration.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+        public ValuesController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var value = configuration["mysecret"];
+            var value2 = configuration["Message"];
+            return Ok("Value for Secret is : " + value + " " + value2);
+        }
+    }
+}
